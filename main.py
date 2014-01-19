8ea88c259cc5e36e5645c7a75cdf9f12a38ae343
sophia <scastellarin95@gmail.co>
2014-01-19 10:34:21 -0500

    small bug fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import fantasy
+#import fantasy
 import pygame
         
 SCREEN_WIDTH = 800
@@ -7,8 +7,8 @@ WHITE = (255,255,255)
 RED = (255,0,0)
 GREEN = (0,255,0)
 
-def is_clicked(btn_x, btn_y, mou_x, mou_y):
-    if (mou_x > btn_x) and (mou_x < btn_x+200) and (mou_y > btn_y) and (mou_y < btn_y+100):
+def is_clicked(btn_x, btn_y, mou_x, mou_y, btn_w, btn_h):
+    if (mou_x > btn_x) and (mou_x < btn_x+btn_w) and (mou_y > btn_y) and (mou_y < btn_y+btn_h):
 	return True
     else:	
 	return False
@@ -19,19 +19,25 @@ def main():
     running = True
     pygame.init()
     size = width, height = SCREEN_WIDTH, SCREEN_HGHT
-    background_base = WHITE 
+    background = WHITE 
     scr = pygame.display.set_mode(size)
 
-    new_game_btn = pygame.image.load('res/new_game_btn.png').convert()
-    quit_btn = pygame.image.load('res/quit_btn.png').convert()
+    new_game_img=('res/new_game_btn.png')
+    quit_img=('res/quit_btn.png')
+
+    new_game_btn = pygame.image.load(new_game_img).convert()
+    quit_btn = pygame.image.load(quit_img).convert()
     ngb_pos = (200, 200)
     qb_pos = (200, 340)
+
+    btn_width = new_game_btn.get_size()[0]	# Buttons are the same size
+    btn_height = new_game_btn.get_size()[1]
 
     clock = pygame.time.Clock()
 
     while running == True:
 	clock.tick(60)
-	scr.fill(WHITE)
+	scr.fill(background)
 	scr.blit(new_game_btn, ngb_pos)
 	scr.blit(quit_btn, qb_pos) 
 	pygame.display.flip()
@@ -44,11 +50,11 @@ def main():
 		    running = False
 	    elif event.type == pygame.MOUSEBUTTONDOWN:
 		m = pygame.mouse.get_pos()
-		if is_clicked(ngb_pos[0], ngb_pos[1], m[0], m[1]):
-		    game = fantasy.Core()
-		    game.on_execute()
-		if is_clicked(qb_pos[0], qb_pos[1], m[0], m[1]):
-		    running = False
-
-
+		if is_clicked(ngb_pos[0], ngb_pos[1], m[0], m[1], btn_width, btn_height):
+#		    game = fantasy.Core()
+#		    game.on_execute()
+                   background = GREEN
+		if is_clicked(qb_pos[0], qb_pos[1], m[0], m[1], btn_width, btn_height):
+#		    running = False
+                   background = RED
 main()
